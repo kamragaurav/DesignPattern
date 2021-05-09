f0352e5c0f5a825ed7d57594fbca605a67cf39d1
Gaurav Kamra <kamra.gaurav515@gmail.com>
2021-05-10 00:14:23 +0530

    breaking singleton through reflection

--- src/designpattern/creational/SingletonDemo.java
+++ src/designpattern/creational/SingletonDemo.java
@@ -1,32 +1,49 @@
 package designpattern.creational;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 class Singleton {
 
+    public static volatile Singleton INSTANCE;
     private Singleton(){}
 
-    private static class InstanceHolder{
-        public static volatile Singleton INSTANCE=new Singleton();
+    public static Singleton getInstance(){
+        if(INSTANCE==null){
+            INSTANCE = new Singleton();
+        }
+        return INSTANCE;
     }
 
-    public static Singleton getInstance(){
-        return InstanceHolder.INSTANCE;
-    }
+
 }
 public class SingletonDemo {
-    static  void useSingleton(){
-        Singleton s1 = Singleton.getInstance();
-        print("s1",s1);
-    }
-    public static void main(String[] args) {
-        ExecutorService service = Executors.newFixedThreadPool(4);
-        service.submit(SingletonDemo::useSingleton);
-        service.submit(SingletonDemo::useSingleton);
-        service.submit(SingletonDemo::useSingleton);
-        service.shutdown();
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InvocationTargetException, InstantiationException {
+       // Class aClass = Class.forName("designpattern.creational.Singleton");
+        //Constructor<Singleton>[] constructor = aClass.getDeclaredConstructors();
+        Object[] parameters = new Object[3];
+        Singleton instance2;
+        Constructor constructors =
+                Singleton.class.getDeclaredConstructors()[0];
+        //for (Constructor constructor : constructors)
+        //{
+            // Below code will destroy the singleton pattern
+            constructors.setAccessible(true);
+            instance2 = (Singleton) constructors.newInstance(null);
+            Singleton instance = Singleton.getInstance();
+            print("s",instance2);
+        print("s1",instance);
+          //  break;
+        //}
+
+
+
+
+
     }
 
     public static void print(String name, Object obj){
