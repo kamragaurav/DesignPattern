d94940e5d338db525d9981bf5593eee4a58272a5
Gaurav Kamra <kamra.gaurav515@gmail.com>
2021-05-09 22:03:46 +0530

    Breaking Singleton using Clone

--- src/designpattern/creational/SingletonDemo.java
+++ src/designpattern/creational/SingletonDemo.java
@@ -1,5 +1,5 @@
 package designpattern.creational;
-class Singleton{
+class Singleton implements Cloneable{
     private static Singleton INSTANCE;
     private Singleton(){}
     public static Singleton getInstance(){
@@ -8,13 +8,20 @@ class Singleton{
        }
        return INSTANCE;
     }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
 public class SingletonDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException {
         Singleton s1 = Singleton.getInstance();
         Singleton s2 =  Singleton.getInstance();
         print("s1",s1);
         print("s2",s2);
+        Singleton s3 = (Singleton) s2.clone();
+        print("s3",s3);
     }
 
     public static void print(String name, Object obj){
